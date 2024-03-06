5da1cc6729226dcd94aa2c77ce2879bc3742d872
Rafael <fael.caetsantos@gmail.com>
2024-03-06 18:58:03 -0300

    feat: add darkmode and socialmedia icon

--- app.vue
+++ app.vue
@@ -1,15 +1,21 @@
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      >
-        <!-- left sidebad -->
-        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-          left
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left sidebad -->
+          <div
+            class="border-2 border-red-500 md:block xs-col-span-1 xl:col-span-2"
+          >
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Main content -->
+          <!-- Right sidebar -->
         </div>
-        <!-- Min content -->
-        <!-- right sidebar -->
       </div>
     </div>
   </div>
---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="w-8 h-8"></div>
+    <LogoSocialMedia />
+  </div>
+</template>
+<script setup></script>
