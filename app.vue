9c91575d420fe5e306a40f60a53f4cdc6533121d
Rafael <fael.caetsantos@gmail.com>
2024-03-06 15:32:53 -0300

    feat: setup tailwind and grid screen

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{ dark: darkMode }">
+    <div class="min-h-full">
+      <div
+        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+      >
+        <!-- left sidebad -->
+        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
+          left
+        </div>
+        <!-- Min content -->
+        <!-- right sidebar -->
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const darkMode = ref(true);
+</script>
